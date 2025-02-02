fdbfa1fb045b29daca692d895ac533a8d6866897
igr76 <gritsuk766@gmail.com>
2025-02-02 18:38:13 +1000

    start

---
+++ src/test/java/org/example/kafkaex/KafkaExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kafkaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
